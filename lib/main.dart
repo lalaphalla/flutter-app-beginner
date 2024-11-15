ce64e76c1f03fbb0845068c5342bae3eca6af2a5
Pav Munyphalla <lalaphalla@gmail.com>
2024-11-15 21:09:19 +0700

    feat add

--- lib/main.dart
+++ lib/main.dart
@@ -97,31 +97,60 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class AppBarApp extends StatelessWidget{
+class AppBarApp extends StatelessWidget {
   const AppBarApp({super.key});
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return const MaterialApp(
       home: AppBarExample(),
     );
   }
 }
 
-class AppBarExample extends StatelessWidget{
+class AppBarExample extends StatelessWidget {
   const AppBarExample({super.key});
 
   @override
-  Widget build(BuildContext context) { 
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('AppBar Demo'),
-        actions: <Widget>[
-          IconButton(
-            onPressed: (){
-              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Here is snackbar')));},
-              icon: const Icon(Icons.add_alert))
-        ]
+      appBar: AppBar(title: const Text('AppBar Demo'), actions: <Widget>[
+        IconButton(
+            onPressed: () {
+              ScaffoldMessenger.of(context).showSnackBar(
+                  const SnackBar(content: Text('Here is snackbar')));
+            },
+            tooltip: 'Show Snackbar',
+            icon: const Icon(Icons.add_alert)),
+        IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                return const MyApp();
+              }));
+            },
+            tooltip: 'Go to next page',
+            icon: const Icon(Icons.navigate_next)),
+        IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (BuildContext context) {
+                return const AppList();
+              }));
+            },
+            tooltip: 'Go to next page',
+            icon: const Icon(Icons.navigation_rounded)),
+      ]),
+      body: const SingleChildScrollView(
+        child: Column(
+          children: [
+            TitleSection(
+              name: 'Oeschinen Lake Campground',
+              location: 'Kandersteg, Switzerland',
+            ),
+            ButtonSection(),
+          ],
+        ),
       ),
     );
   }
@@ -136,6 +165,145 @@ class PaddedText extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: Text(text),
+    );
+  }
+}
+
+class AppList extends StatelessWidget {
+  const AppList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const title = 'Shoping List';
+
+    return MaterialApp(
+        title: title,
+        home: Scaffold(
+            appBar: AppBar(title: const Text(title)),
+            body: ListView(children: const <Widget>[
+              ListTile(
+                leading: Icon(Icons.map),
+                title: Text('Map'),
+              ),
+              ListTile(
+                leading: Icon(Icons.photo_album),
+                title: Text('Photo'),
+              ),
+              ListTile(
+                leading: Icon(Icons.phone),
+                title: Text('Phone'),
+              ),
+            ])));
+  }
+}
+
+// Create Button Widget
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ButtonWithText(
+            color: color,
+            icon: Icons.call,
+            label: 'CALL',
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.near_me,
+            label: 'ROUTE',
+          ),
+          ButtonWithText(
+            color: color,
+            icon: Icons.share,
+            label: 'SHARE',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  const ButtonWithText({
+    super.key,
+    required this.color,
+    required this.icon,
+    required this.label,
+  });
+
+  final Color color;
+  final IconData icon;
+  final String label;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, color: color),
+        Padding(
+          padding: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                // 2
+                Padding(
+                    padding: const EdgeInsets.only(bottom: 8),
+                    child: Text(
+                      name,
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    )),
+                Text(
+                  location,
+                  style: TextStyle(color: Colors.grey[500]),
+                )
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Colors.red[500]),
+          const Text('41')
+        ],
+      ),
     );
   }
 }
